d7f1384d94470be9aaf99df9b59e0fa6abc796d4
kothru <1894683+kothru@users.noreply.github.com>
2022-03-21 23:24:29 +0900

    add apollo client

---
+++ README.md
@@ -0,0 +1,13 @@
+# apollo-client sample
+
+## ref
+
+https://www.apollographql.com/docs/react/get-started
+
+## start server
+
+npm run dev
+
+## out url
+
+http://localhost:3000/
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
 
+const GET_BOOKS = gql`
+  query GetBooks {
+    books {
+      title
+      author
+    }
+  }
+`;
+
+function Books() {
+  const { loading, error, data } = useQuery(GET_BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.books.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {author}: {title}
+      </p>
+    </div>
+  ));
+}
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Books />
     </div>
   )
 }
